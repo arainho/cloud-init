ccaa42ac1a538e6ebc9bdc85bf230d566e31d8d3
André Rainho <arainho.it@gmail.com>
2015-09-11 14:24:08 +0100

    add tenant openrc for fish shell

---
+++ Tenant-openrc.fish
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set OS_USERNAME your-username
+set OS_PASSWORD your-password
+set OS_TENANT_NAME the-tenant-name
+set OS_TENANT_ID the-tenant-id
+set OS_AUTH_URL http://controller-ip:5000/v2.0
