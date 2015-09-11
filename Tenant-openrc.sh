e3c0765a8b11e29829111ae4a6b9089b329d3374
André Rainho <arainho.it@gmail.com>
2015-09-11 13:44:35 +0100

    add tenant openrc template file

---
+++ Tenant-openrc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export OS_USERNAME=your-username
+export OS_PASSWORD=your-password
+export OS_TENANT_NAME=the-tenant-name
+export OS_TENANT_ID=the-tenant-id
+export OS_AUTH_URL=http://controller-ip:5000/v2.0
