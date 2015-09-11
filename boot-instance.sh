133974fa27811bc4c719c6a60605a5b0177db0ff
André Rainho <arainho.it@gmail.com>
2015-09-11 14:26:13 +0100

    Check shell type and fix 'IMAGE_ID=' line

--- boot-instance.sh
+++ boot-instance.sh
@@ -6,12 +6,30 @@ KEY_NAME="arainho-terrance"                     # your key name
 ZONE="zone1"                                    # zone name
 USER_DATA_NAME="openfire-instance.txt"          # your user-data file, inside user-data folder
 INSTANCE_NAME="openfire"                        # the instance name
-CREDS_FILE="Tenant-openrc.sh"                   # tenant credentials file
+CREDS_FILE="Tenant-openrc.sh"                   # tenant credentials file ( bash shell )
+CREDS_FILE_FISH_SHELL="Tenant-openrc.fish"      # tenant credentials file ( fish shell )
 
-source $CREDS_FILE
-IMAGE_ID=$(nova list --all-tenants=1 | grep "$IMAGE_NAME " | awk '{print $2}')
+# check shell type
+if [ "$SHELL" == "/bin/bash" ]
+then
+    source "$CREDS_FILE"
+elif [ "$SHELL" == "/usr/bin/fish" ]
+then
+    . "$CREDS_FILE_FISH_SHELL"
+fi
+
+# get image id
+echo -n "checking image-id ...  "
+IMAGE_ID=$(glance image-list | grep "$IMAGE_NAME " | awk '{print $2}')
+echo "image-id: $IMAGE_ID"
+
+# get flavor id
+echo -n "checking flavor-id ...  "
 FLAVOR_ID=$(nova flavor-list | grep "$FLAVOR_NAME" | awk '{print $2}')
+echo "flavor-id: $FLAVOR_ID"
+
+# set user data
 USER_DATA="./user-data/$USER_DATA_NAME"
 
-echo "nova boot --image "$IMAGE_ID"  --key-name "$KEY_NAME" --flavor "$FLAVOR_ID" --availability-zone "$ZONE" --user-data "$USER_DATA" "$INSTANCE_NAME""
-
+# boot instance
+nova boot --image "$IMAGE_ID"  --key-name "$KEY_NAME" --flavor "$FLAVOR_ID" --availability-zone "$ZONE" --user-data "$USER_DATA" "$INSTANCE_NAME"
